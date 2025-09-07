17c08602be2885790985f52a192474e882634c83
vdW86 <23431818+vdW86@users.noreply.github.com>
2025-09-07 20:37:44 +0200

    Update software.sh

--- software.sh
+++ software.sh
@@ -1,71 +1,29 @@
-sudo dnf install  
-###################
+ ###################
 ##### windowmanager & utils
 ###################
-hyprland \
-waybar \
-dunst \
-xdg-desktop-portal-hyprland \
-xdg-desktop-portal-gtk \
-swaybg \
-pactl \
-brightnessctl \
-wayland-devel \
-polkit \
-sddm \
-xdg-user-dirs \
+sudo dnf install hyprland waybar dunst xdg-desktop-portal-hyprland xdg-desktop-portal-gtk swaybg pactl brightnessctl wayland-devel polkit sddm xdg-user-dirs 
 ###################
 ##### Browsers
 ###################
-firefox \
-chromium \
-qutebrowser \
+sudo dnf install firefox chromium qutebrowser 
 ##### Office  
-mupdf \
-galculator \
-libreoffice \
-libreoffice-langpack-nl \
-geany \
-calibre \
+sudo dnf install mupdf galculator libreoffice libreoffice-langpack-nl geany calibre 
 ###################
 #### Utils
 ###################
-kitty \
-foot \
-wl-clipboard \
-grim \
-slurp \
-rofi \
-thunar \
-thunar-archive-plugin \
-thunar-media-tags-plugin \
-thunar-volman \
-fastfetch \
-speedtest-cli \
-keepassxc \
-syncthing \
+sudo dnf install kitty foot wl-clipboard grim slurp rofi thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman fastfetch speedtest-cli keepassxc syncthing 
 ###################
 ##### Connections
 ###################
-network-manager-applet \
-NetworkManager-gnome \
-bluez \
-bluez-tools \
-blueman \
+sudo dnf install network-manager-applet NetworkManager-gnome bluez bluez-tools blueman 
 ###################
 #### Media
 ###################
-gimp \
-gimp-heif-plugin \
-pinta \
-cheese \
-vlc \
-swayimg \
+sudo dnf install gimp gimp-heif-plugin pinta cheese vlc swayimg 
 ###################
 #### Fonts
 ###################
-fira-code-fonts \
-papirus-icon-theme \
+fira-code-fonts papirus-icon-theme 
 ###################
 #### Extra's
 ###################
