c66c4c44e0eb956342f2ad8087154a9aee7c2e99
vdW86 <23431818+vdW86@users.noreply.github.com>
2025-09-07 20:03:19 +0200

    Update software.sh

--- software.sh
+++ software.sh
@@ -1,32 +1,79 @@
-sudo dnf install hyprland \
+sudo dnf install \ 
+###################
+##### windowmanager & utils
+###################
+hyprland \
 waybar \
 dunst \
+xdg-desktop-portal-hyprland \
+xdg-desktop-portal-gtk \
+swaybg \
+pactl \
+brightnessctl \
+wayland-devel \
+polkit \
+sddm \
+xdg-user-dirs \
+###################
+##### Browsers
+###################
+firefox \
+chromium \
+qutebrowser \
+##### Office  
+mupdf \
+galculator \
+libreoffice \
+libreoffice-langpack-nl \
+geany \
+calibre \
+###################
+#### Utils
+###################
 kitty \
+foot \
 wl-clipboard \
 grim \
 slurp \
-firefox \
 rofi \
-xdg-desktop-portal-hyprland \
-xdg-desktop-portal-gtk \
-network-manager-applet \
-fira-code-fonts \
-papirus-icon-theme \
 thunar \
 thunar-archive-plugin \
 thunar-media-tags-plugin \
 thunar-volman \
-pactl \
-brightnessctl \
-swaybg \
 fastfetch \
 speedtest-cli \
+keepassxc \
+syncthing \
+###################
+##### Connections
+###################
+network-manager-applet \
+NetworkManager-gnome \
+bluez \
+bluez-tools \
+blueman \
+###################
+#### Media
+###################
+gimp \
+gimp-heif-plugin \
+pinta \
+cheese \
 vlc \
-qutebrowser \
-wayland-devel \
-polkit \
-sddm \
-xdg-user-dirs
-
+swayimg \
+###################
+#### Fonts
+###################
+fira-code-fonts \
+papirus-icon-theme \
+###################
+#### Extra's
+###################
+# Flatpak inschakelen
+####
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+####
 sudo systemctl enable sddm.service
+sudo systemctl enable bluetooth.service
+####
 xdg-users-dirs-update
